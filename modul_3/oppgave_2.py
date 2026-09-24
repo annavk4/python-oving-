3e63c379171c7ce7bdc3a7ddab6488b80aca6082
annavk4 <annavk@uia.no>
2026-09-24 17:22:47 +0200

    Fullført oppgave 2

--- modul_3/oppgave_2.py
+++ modul_3/oppgave_2.py
@@ -1,1 +1,14 @@
+navn = "Michael"
+alder = 31
 
+print("Hei, jeg heter", navn)
+print("Jeg er", alder, "år gammel")
+print("Ha det!")
+
+#Refleksjon 
+
+#Spørsmål 1: Feilene var manglende anførselstegn rundt navn, print skrevet feil, manglende anførselstegn på sluten og glemt sluttparantes.
+
+#Spørsmål 2: Jeg syns den vanskligste var å komme på at Michael må ha anførselstegn siden det er tekst
+
+#Spørsmål 3: Jeg tror Python peker på linjen der den oppdager problemet, men selve feilen kan være på linjen før.
