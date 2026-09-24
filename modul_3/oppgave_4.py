6b46da24575381cf9bd418de87f5edb9f3e75f46
annavk4 <annavk@uia.no>
2026-09-24 18:06:40 +0200

    Fullført oppgave 4

--- modul_3/oppgave_4.py
+++ modul_3/oppgave_4.py
@@ -1,1 +1,29 @@
+poeng = 0 
 
+# Jeg tror poeng blir: 10 
+poeng = poeng + 10
+print(poeng) 
+
+# Jeg tror poeng blir 35 
+poeng += 25
+print(poeng) 
+
+# Jeg tror poeng blir 30
+poeng -= 5
+print(poeng)
+
+# Jeg tror poeng blir 60
+poeng *= 2
+print(poeng)
+
+# Jeg tror poeng blir 15
+poeng /= 4 
+print(poeng)
+
+#Refleksjon 
+
+#Spørsmål 1: Alle stemte. Tenkte at det ikke nullstilles mellom hver gang.
+
+#Spørsmål 2: Når vi delte ble datatypen om til float, som er med desimaltall. Det er vanlig når man skal dele.
+
+#Spørsmål 3: Ja, det ga helt samme resultat. Jeg tenker at man bruker tegn fordi det blir som en forkortelse. 
