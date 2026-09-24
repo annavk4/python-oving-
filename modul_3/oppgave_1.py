bdf530a0e4bbd8499ee97fc0e2ae8210bae622b4
annavk4 <annavk@uia.no>
2026-09-24 17:05:44 +0200

    Fullført oppgave 1

--- modul_3/oppgave_1.py
+++ modul_3/oppgave_1.py
@@ -1,1 +1,31 @@
+navn = "Anna"
+alder = 21
+høyde = 1.70
+er_student = True
+hobbyer = ["trene","bake","være sosial"]
 
+print("Navn:", navn)
+print("Alder:", alder)
+print("Høyde:", høyde)
+print("Er student:", er_student)
+print("Hobbyer:", hobbyer)
+
+print("Hobbyer:", hobbyer)
+print("Er student:", er_student)
+print("Høyde:", høyde)
+print("Alder:", alder)
+print("Navn:", navn)
+
+print(type(navn))
+print(type(alder))
+print(type(høyde))
+print(type(er_student))
+print(type(hobbyer))
+
+#Refleksjon 
+
+#Spørsmål 1: For å skrive ut variablene i motsatt rekkefølge skrev jeg bare print()-linjene på nytt i den nye rekkefølgen. Jeg trengte ikke å endre variablene øverst.
+
+#Spørsmål 2: Python skrev ned datatypene str, int, float, bool og list. Det stemmer med det vi har lært fra pensum. 
+
+#Spørsmål 3: Jeg tror det er viktig at pyhton vet hva slags datatype det er for å kunne jobbe videre med variablene. For eksempel med å regne med tall og sette sammen en tekst.
